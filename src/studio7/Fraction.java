8ecfac166d5a27a1fe9eaf151b8ba49cff29dfea
calvinchao <calvinchao@localhost>
2024-10-30 14:40:10 -0500

    Calvin CHao- 10/30/24 Updates

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -1,10 +1,27 @@
 package studio7;
 
 public class Fraction {
+	
+	private int numerator;
+	private int denominator;
+	
+	public Fraction(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+	}
+	
+	public Fraction add (Fraction F2) {
+		int newNumerator;
+		int newDenominator;
+		newDenominator = this.denominator * F2.denominator;
+		newNumerator = (this.numerator * F2.denominator) + (F2.numerator * this.denominator);
+		return new Fraction(newNumerator, newDenominator) ;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Fraction newFraction = add(F1, F2); 
+		System.out.println(NewFraction.numerator + "/" + newFraction.denomennator)
 	}
 
 }
